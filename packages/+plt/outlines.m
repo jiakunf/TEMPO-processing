22f7f09cb3f25b8212cfb91e529bba1a61a1d652
Vasily <OldBasilio@gmail.com>
2024-11-20 16:09:51 -0700

    no outline plotting outside plot limits

--- packages/+plt/outlines.m
+++ packages/+plt/outlines.m
@@ -1,5 +1,17 @@
-function outlines(countors, varargin)
+function outlines(countors, x_lim, y_lim, varargin)
+    if(nargin < 2) xlim = []; end
+    if(nargin < 3) ylim = []; end    
+
     tf = ishold;
+    
+    for i_r = 1:size(countors, 3)
+        countors(countors(:,1,i_r) < x_lim(1), 1, i_r) = NaN;
+        countors(countors(:,1,i_r) > x_lim(2), 1, i_r) = NaN;
+        countors(countors(:,2,i_r) < y_lim(1), 1, i_r) = NaN;
+        countors(countors(:,2,i_r) > y_lim(2), 1, i_r) = NaN;
+        % plot(countors(:,1,i_r), countors(:,2,i_r), varargin{:}); hold on;
+    end
+
     for i_r = 1:size(countors, 3)
         plot(countors(:,1,i_r), countors(:,2,i_r), varargin{:}); hold on;
     end
