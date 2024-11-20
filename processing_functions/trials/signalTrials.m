ce904d0732848521f95344a7479b7b2b9ee26c24
Vasily <OldBasilio@gmail.com>
2024-11-20 16:41:19 -0700

    better handling of dropped ttl

--- processing_functions/trials/signalTrials.m
+++ processing_functions/trials/signalTrials.m
@@ -28,7 +28,8 @@ function [X_trials, window_stim, intervals_stim] = signalTrials(X, ttl, varargin
     intervals = ttlGetIntervals(ttl, options.min_stim_length);
     
     sti = round(median(intervals{2}(:,3)));
-    iti = round(median(intervals{1}(:,3))*options.iti_scale);
+    % iti = round(median(intervals{1}(:,3))*options.iti_scale);
+    iti = round(sti*options.iti_scale);
     
     window_stim = [repelem(0, iti), ...
                    repelem(1, sti),...
@@ -36,16 +37,32 @@ function [X_trials, window_stim, intervals_stim] = signalTrials(X, ttl, varargin
     window_length = length(window_stim);
     
     X_trials = zeros([size(intervals{2},1)-2*options.drop, size(X,1), window_length]);
-    ttl_trials = zeros([size(intervals{2},1)-2*options.drop, size(X,1), window_length]);
+    % ttl_trials = zeros([size(intervals{2},1)-2*options.drop, size(X,1), window_length]);
     
     intervals_stim = intervals{2}((options.drop+1):(size(intervals{2},1)-options.drop),:);
     
-    for i_s = (1+options.drop):(size(intervals{2}, 1)-options.drop) 
+    % for i_s = (1+options.drop):(size(intervals{2}, 1)-options.drop) 
+    dropL = 0;
+    dropR = 0;
+    for i_s = 1:(size(intervals{2}, 1)) 
         f_start = intervals{2}(i_s,2)-iti+...
                  (intervals{2}(i_s,3)-sti)*options.align_to_end;
-        X_trials(i_s-options.drop,:,:) = X(:,f_start:(f_start+window_length-1));
+        f_end = (f_start+window_length-1);
+
+        if(f_start < 1) 
+            dropL = dropL+1; 
+            continue;
+        end
+        if(f_end > size(X,2)) 
+            dropR = dropR+1; 
+            continue;
+        end       
+
+        X_trials(i_s-options.drop,:,:) = X(:,f_start:f_end);
     end 
     %%
+
+    X_trials = X_trials((dropL+1):(end-dropR),:,:);
     
     X_trials = reshape(X_trials, ...
         [size(X_trials,1),sz_in(1:(ndims_in-1)),size(X_trials,3)]);
