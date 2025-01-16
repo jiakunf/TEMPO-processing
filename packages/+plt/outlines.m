9845fb670046f078b8301978bd8b094c62052336
Vasily <OldBasilio@gmail.com>
2025-01-15 20:46:23 -0800

    fixed outlines plotting limits

--- packages/+plt/outlines.m
+++ packages/+plt/outlines.m
@@ -1,15 +1,14 @@
 function outlines(countors, x_lim, y_lim, varargin)
-    if(nargin < 2) xlim = []; end
-    if(nargin < 3) ylim = []; end    
+    if(nargin < 2) x_lim = [-Inf,Inf]; end
+    if(nargin < 3) y_lim = [-Inf,Inf]; end    
 
-    tf = ishold;
+    tf = ishold();
     
     for i_r = 1:size(countors, 3)
         countors(countors(:,1,i_r) < x_lim(1), 1, i_r) = NaN;
         countors(countors(:,1,i_r) > x_lim(2), 1, i_r) = NaN;
         countors(countors(:,2,i_r) < y_lim(1), 1, i_r) = NaN;
         countors(countors(:,2,i_r) > y_lim(2), 1, i_r) = NaN;
-        % plot(countors(:,1,i_r), countors(:,2,i_r), varargin{:}); hold on;
     end
 
     for i_r = 1:size(countors, 3)
